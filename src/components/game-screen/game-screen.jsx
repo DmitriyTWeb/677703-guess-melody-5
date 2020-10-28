bfd55b0e18a3afc9907234f17c4d7278e927a2bf
Dmitriy Teterin <teterinda@gmail.com>
2020-10-28 18:25:06 +0300

    Подключим компонент Mistakes в GameScreen

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
@@ -6,6 +6,7 @@ import {ActionCreator} from "../../store/action.js";
 import {GameType} from "../../const.js";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import Mistakes from "../mistakes/mistakes";
 
 import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player";
 
@@ -13,13 +14,19 @@ const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
 const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
 
 const GameScreen = (props) => {
-  const {questions, step, onUserAnswer, resetGame} = props;
+  const {
+    questions,
+    step,
+    onUserAnswer,
+    resetGame,
+    mistakes,
+  } = props;
   const question = questions[step];
 
   if (step >= questions.length || !questions) {
     resetGame();
 
-    return(
+    return (
       <Redirect to="/" />
     );
   }
@@ -30,14 +37,18 @@ const GameScreen = (props) => {
         <ArtistQuestionScreenWrapped
           question={question}
           onAnswer={onUserAnswer}
-        />
+        >
+          <Mistakes count={mistakes} />
+        </ArtistQuestionScreenWrapped>
       );
     case GameType.GENRE:
       return (
         <GenreQuestionScreenWrapped
           question={question}
           onAnswer={onUserAnswer}
-        />
+        >
+          <Mistakes count={mistakes} />
+        </GenreQuestionScreenWrapped>
       );
   }
 
@@ -48,11 +59,13 @@ GameScreen.propTypes = {
   questions: PropTypes.array.isRequired,
   step: PropTypes.number.isRequired,
   resetGame: PropTypes.func.isRequired,
-  onUserAnswer: PropTypes.func.isRequired
+  onUserAnswer: PropTypes.func.isRequire,
+  mistakes: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   step: state.step,
+  mistakes: state.mistakes
 });
 
 const mapDispatchToProps = (dispatch) => ({
